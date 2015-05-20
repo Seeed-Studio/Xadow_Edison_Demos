8ec0a15c469fb23c91eadd30bd7769071d3ecd89
Lambor <757016515@qq.com>
2015-05-20 22:04:34 +0800

    Update Cool_Light_WriteTag.ino

--- Cool_Light_WriteTag/Cool_Light_WriteTag.ino
+++ Cool_Light_WriteTag/Cool_Light_WriteTag.ino
@@ -13,8 +13,8 @@ NfcAdapter nfc = NfcAdapter(pn532spi);
 #include <PN532.h>
 #include <NfcAdapter.h>
 
-//change the number after colon like "0" or "1" or "2".
-#define WRITE_TAG_MESSAGE "color:0"  
+//change the number after colon like "1" or "2" or "3".
+#define WRITE_TAG_MESSAGE "color:1" 
 
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
@@ -28,6 +28,9 @@ void setup() {
 
 void loop() {
     Serial.println("\nPlace a formatted Mifare Classic NFC tag on the reader.");
+    Serial.print("Writing message: <");
+    Serial.print(WRITE_TAG_MESSAGE);
+    Serial.println("> to you NFC tag");
     if (nfc.tagPresent()) {
         NdefMessage message = NdefMessage();
         message.addTextRecord(WRITE_TAG_MESSAGE);
